a1374ec5a4d0126ae02cd594d65c2d9a451f9354
civenn <141903624+civenn@users.noreply.github.com>
2023-12-09 13:39:16 +1100

    bug
    fixed initializing bug, no fucking idea how

--- src/Weather.js
+++ src/Weather.js
@@ -19,9 +19,6 @@ export default function Weather() {
       .catch((error) => {
         setError(true);
         setTemperature(null);
-        setWind(null);
-        setHumidity(null);
-        setDescription(null);
       });
   }
 
