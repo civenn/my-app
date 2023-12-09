0ab2161f2aabcc53babbf7f6b564c9ad2faa203f
civenn <141903624+civenn@users.noreply.github.com>
2023-12-09 13:07:04 +1100

    import app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <h1>Search Weather</h1>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let [temperature, setTemperature] = useState(null);
+  let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios
+      .get(url)
+      .then((response) => {
+        setTemperature(response.data.main.temp);
+        setError(false);
+      })
+      .catch((error) => {
+        setError(true);
+        setTemperature(null);
+        setWind(null);
+        setHumidity(null);
+        setDescription(null);
+      });
+  }
+
+  function handleInputChange(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {temperature !== null && !error && (
+        <p>
+          The weather in {city} is:
+          <ul>
+            <li> Temperature: {Math.round(temperature)}°C </li>
+          </ul>
+        </p>
+      )}
+      {error && <p>Error: Unable to fetch the temperature for {city}</p>}
+    </div>
+  );
+}
